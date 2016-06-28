e35bf0f89ce5278eee6f46150052d37586d237a9
bur <bur@bur-macbookpro.roam.corp.google.com>
2016-06-28 11:46:00 -0400

    Header update

--- Crypt/Mechanisms/Enablement.swift
+++ Crypt/Mechanisms/Enablement.swift
@@ -1,8 +1,7 @@
 /*
-  Enablement.swift
   Crypt
 
-  Copyright 2015 The Crypt Project.
+  Copyright 2016 The Crypt Project.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
