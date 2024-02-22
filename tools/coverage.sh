8cd80d73cf1f63513a432e6f5a7fad405475a3b5
Graham Gilbert <graham@grahamgilbert.com>
2024-02-22 13:19:30 -0800

    goroot

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-go get -u github.com/jandelgado/gcov2lcov
-go install github.com/jandelgado/gcov2lcov
+go install github.com/jandelgado/gcov2lcov@latest
 rm -rf coverage
 mkdir -p coverage
 # bazel coverage --combined_report=lcov //...
 # mv $(BAZEL_OUTPUT_PATH)/_coverage/_coverage_report.dat coverage/lcov.info
 go test -coverprofile=coverage/coverage.out ./...
 go tool cover -func=coverage/coverage.out -o coverage/coverage.txt
-gcov2lcov -infile=coverage/coverage.txt -outfile=coverage/lcov.info
+GOROOT=$(go env GOROOT) gcov2lcov -infile=coverage/coverage.txt -outfile=coverage/lcov.info
