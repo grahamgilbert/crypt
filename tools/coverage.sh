2854eaad01561ae9ff501456a57dcf2bd84a2f9d
Graham Gilbert <graham@grahamgilbert.com>
2024-02-22 13:06:15 -0800

    Let's try this

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-
+go get github.com/jandelgado/gcov2lcov
 rm -rf coverage
 mkdir -p coverage
 # bazel coverage --combined_report=lcov //...
 # mv $(BAZEL_OUTPUT_PATH)/_coverage/_coverage_report.dat coverage/lcov.info
-go test -coverprofile=coverage/lcov.info ./...
+go test -coverprofile=coverage/coverage.out ./...
+go tool cover -func=coverage/coverage.out -o coverage/coverage.txt
+gcov2lcov -infile=coverage/coverage.txt -outfile=coverage/lcov.info
